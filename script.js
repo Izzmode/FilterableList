a743a7be71f5958cb4e0f4a4e5d77331fa0ef934
Izabelle Olofsson <izabelleolofsson@live.se>
2023-02-05 15:34:57 +0100

    made so you can add a name to contact-list

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let filterInput = document.querySelector('#filterInput');
+const form = document.querySelector('form');
 
 const filterNames = (e) => {
 let filterValue = filterInput.value.toUpperCase();
@@ -22,3 +23,74 @@ for (let i = 0; i < li.length; i++){
 }
 
 filterInput.addEventListener('keyup', filterNames);
+
+
+
+//SKAPA KONTAKT
+
+// const addContact = (e) => {
+//     e.preventDefault();
+//     const collectionHeader = document.querySelector('.collection-header');
+//     let contactName = form.querySelector('#contactName').value;
+//     let contactNumber = form.querySelector('#contactNumber').value;
+
+//     console.log(collectionHeader.firstElementChild.innerText.indexOf('A'))
+//     console.log(contactName.indexOf('A'))
+
+    
+//     if (collectionHeader.firstElementChild.innerText.indexOf('A') == contactName.indexOf('A')) {
+//         //skapar Li
+//     const listItem = document.createElement('li');
+//     listItem.classList.add('collection-item');
+    
+//     //skapar a
+//     const aLink = document.createElement('a');
+//     aLink.innerText = contactName;
+    
+//     //lägger a-taggen i li
+//     listItem.appendChild(aLink);
+
+//     // tbd fixa bättre append
+//     collectionHeader.appendChild(listItem);
+
+
+
+// }
+
+// }
+
+// form.addEventListener('submit', addContact);
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let contactName = form.querySelector('#contactName').value;
+    let contactNumber = form.querySelector('#contactNumber').value;
+    letters = document.querySelectorAll('ul h2');
+
+    letters.forEach(header => {
+        console.log(contactName[0].toUpperCase())
+        console.log(header.innerHTML)
+        if (header.innerHTML == contactName[0].toUpperCase()){
+                        //skapar Li
+        const listItem = document.createElement('li');
+        listItem.classList.add('collection-item');
+        
+        //skapar a
+        const aLink = document.createElement('a');
+        aLink.innerText = contactName;
+        
+        //lägger a-taggen i li
+        listItem.appendChild(aLink);
+
+        // tbd fixa bättre append
+        header.nextElementSibling.appendChild(listItem);
+        }
+    
+    });
+
+    form.reset();
+
+});
+
+
